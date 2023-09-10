35b48f1b01941fee3f0686a2c7e0255a361fdae1
Pyrexiaa <lim.jia.yu.desmond@gmail.com>
2023-09-10 13:52:30 +0800

    scene2scene

--- ldm/modules/encoders/modules.py
+++ ldm/modules/encoders/modules.py
@@ -133,6 +133,39 @@ class SpatialRescaler(nn.Module):
 
     def encode(self, x):
         return self(x)
+    
+class SpatialRescaler_Scene2Scene(nn.Module):
+    def __init__(self,
+                 n_stages=1,
+                 method='bicubic',
+                 multiplier=0.5,
+                 in_channels=3,
+                 out_channels=None,
+                 bias=False,
+                 RGB_status=False):
+        super().__init__()
+        self.n_stages = n_stages
+        assert self.n_stages >= 0
+        assert method in ['nearest','linear','bilinear','trilinear','bicubic','area']
+        self.multiplier = multiplier
+        self.interpolator = partial(torch.nn.functional.interpolate, mode=method)
+        self.remap_output = out_channels is not None
+        if self.remap_output:
+            print(f'Spatial Rescaler mapping from {in_channels} to {out_channels} channels after resizing.')
+            self.channel_mapper = nn.Conv2d(in_channels,out_channels,1,bias=bias)
+        self.RGB_status = RGB_status
+
+    def forward(self,x):
+        for stage in range(self.n_stages):
+            x = self.interpolator(x, scale_factor=self.multiplier)
+
+        if self.RGB_status == False:
+            if self.remap_output:
+                x = self.channel_mapper(x)
+        return x
+
+    def encode(self, x):
+        return self(x)
 
 
 class FrozenCLIPTextEmbedder(nn.Module):
